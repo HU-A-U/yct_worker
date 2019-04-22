e574e5307909d2aa1326e088d27672f509925ece
胡阿U <44232023+HU-A-U@users.noreply.github.com>
2019-04-22 09:56:26 +0800

    Update celery_config.py

--- handle_data/celery_config.py
+++ handle_data/celery_config.py
@@ -21,25 +21,22 @@ YuanQuToken = 'yuanqu001'
 SURL = "mysql+pymysql://root:159357a@192.168.10.11:3306/yct_proxy?charset=utf8&autocommit=true"
 
 
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 6379
-# REDIS_HOST = '192.168.1.152'
-# REDIS_PORT = 16379
+REDIS_HOST = '192.168.1.152'
+REDIS_PORT = 16379
 
-# RABBITMQ_HOST = '127.0.0.1'
-# RABBITMQ_PORT = 5672
-RABBITMQ_HOST = '192.168.1.152'
+
+RABBITMQ_HOST = '47.102.218.137'
 RABBITMQ_PORT = 5672
 
 
 # 注意，celery4版本后，CELERY_BROKER_URL改为BROKER_URL
 from kombu import Queue, Exchange
 
-# BROKER_URL = 'amqp://cic_admin:JYcxys@3030@{}:{}/yct'.format(RABBITMQ_HOST,RABBITMQ_PORT)
-BROKER_URL = 'amqp://test:test@127.0.0.1:5672/test'
+BROKER_URL = 'amqp://cic_admin:JYcxys@3030@{}:{}/yct'.format(RABBITMQ_HOST,RABBITMQ_PORT)
+
 # 指定结果的接受地址
-# CELERY_RESULT_BACKEND = 'redis://{}:{}/15'.format(REDIS_HOST,REDIS_PORT)
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/15'
+CELERY_RESULT_BACKEND = 'redis://{}:{}/15'.format(REDIS_HOST,REDIS_PORT)
+
 # celery worker的并发数，默认是服务器的内核数目,也是命令行-c参数指定的数目
 CELERYD_CONCURRENCY = 4
 #指定导入的任务模块
