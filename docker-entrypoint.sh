a475581979977b2b48b8dfda572d0c45d2aa94e1
胡阿U <44232023+HU-A-U@users.noreply.github.com>
2019-04-23 11:35:29 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,2 @@
 
-celery multi start to_product -A handle_data worker -l info -Q to_product
-celery multi start to_analysis -A handle_data worker -l info -Q to_analysis
-celery multi start to_consume -A handle_data worker -l info -Q to_consume
-
 /usr/local/bin/mitmdump -s /code/start_script.py
