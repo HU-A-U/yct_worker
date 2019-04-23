9886d567cd2e6315e4a41fc5d5d5c966892252a8
胡阿U <44232023+HU-A-U@users.noreply.github.com>
2019-04-23 16:43:03 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
-celery multi start to_product -A handle_data worker -l info -Q to_product
-celery multi start to_analysis -A handle_data worker -l info -Q to_analysis
-celery multi start to_consume -A handle_data worker -l info -Q to_consume
+celery multi start to_product -A handle_data.celery_app worker -l info -Q to_product
+celery multi start to_analysis -A handle_data.celery_app worker -l info -Q to_analysis
+celery multi start to_consume -A handle_data.celery_app worker -l info -Q to_consume
