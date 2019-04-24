7044aac01da3df3edc635555e02e7c24974d4366
胡阿U <44232023+HU-A-U@users.noreply.github.com>
2019-04-24 09:00:46 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,12 @@
-celery -A handle_data.celery_app worker -l info -Q to_product
-celery -A handle_data.celery_app worker -l info -Q to_analysis
-celery -A handle_data.celery_app worker -l info -Q to_consume
+#!/bin/sh
+set -e
+
+if [ "$QUEUE" = "$P_QUEUE" ]; then
+	celery -A handle_data worker -l info -Q to_product
+elif [ "$QUEUE" = "$A_QUEUE" ]; then
+	celery -A handle_data worker -l info -Q to_analysis
+elif [ "$QUEUE" = "$C_QUEUE" ]; then
+	celery -A handle_data worker -l info -Q to_consume
+else
+	exit 1
+fi
