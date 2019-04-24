9c8890674b92beb53973b51cf52ed70668f84bb1
胡阿U <44232023+HU-A-U@users.noreply.github.com>
2019-04-24 10:29:44 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 if [ "$QUEUE" = "$P_QUEUE" ]; then
-	celery -A handle_data.celery_app worker -l info -Q to_product
+	celery -A handle_data.celery_app worker -l info -Q to_product --uid 13
 elif [ "$QUEUE" = "$A_QUEUE" ]; then
-	celery -A handle_data.celery_app worker -l info -Q to_analysis
+	celery -A handle_data.celery_app worker -l info -Q to_analysis --uid 13
 elif [ "$QUEUE" = "$C_QUEUE" ]; then
-	celery -A handle_data.celery_app worker -l info -Q to_consume
+	celery -A handle_data.celery_app worker -l info -Q to_consume --uid 13
 else
 	exit 1
 fi
